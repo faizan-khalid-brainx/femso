7e97747ca4e3fc22efbb81572f4c2f13b9905d74
FaizanKhalid <faizan.khalid@brainxtech.com>
2021-07-16 11:21:20 +0500

    added login guards to votes

--- src/components/VoteComponent.vue
+++ src/components/VoteComponent.vue
@@ -14,6 +14,7 @@ import axios from 'axios'
 export default {
   props: {
     votes: Number,
+    loginId: Number,
     id: {
       Number,
       required: true
@@ -44,6 +45,7 @@ export default {
       }
     },
     upVote () {
+      console.log(this.loginId)
       if (this.loginId) {
         if (this.downVoted) {
           this.downVoted = false
@@ -61,6 +63,7 @@ export default {
       }
     },
     downVote () {
+      console.log(this.loginId)
       if (this.loginId) {
         if (this.upVoted) {
           this.upVoted = false
